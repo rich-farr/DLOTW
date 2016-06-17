8752cec268557742868ae68fd8c12038f7e2f369
Ollie Bannister <olwiba@gmail.com>
2016-06-17 15:24:35 +1200

    Pushed the data to render page / backend done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var dotenv = require('dotenv');
 var path = require('path');
+var request = require('superagent')
+
 var app = express();
 
 dotenv.load()
@@ -21,11 +23,25 @@ app.get('/', function(req, res) {
 })
 
 app.get('/weather', function (req, res) {
+	var weatherObj = {}
 	if (req.query.name === undefined) {
 	res.render('index')
 	} else {
 		var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + req.query.name + '&units=metric&appid=' + process.env.WEATHER_API_KEY
 		console.log(apiUrl)
+
+		request 
+			.get(apiUrl)
+			.end(function (err, response) {
+				if (!err) {
+					console.log('Rendering...')
+					weatherObj = response.text
+					console.log(weatherObj)
+				} else {
+					console.log('You have an error: ', err)
+				}
+			})
+		res.render('index', weatherObj)
 	}
 	// res.send("Hello this is working :)")
 });
