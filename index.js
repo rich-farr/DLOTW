e9c822ffea02d668511febc91daa6aebda8a9f99
Ollie Bannister <olwiba@gmail.com>
2016-06-17 13:33:46 +1200

    linked in the styles and hbs page

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+
+var path = require('path');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res) {
+ res.redirect('/weather') // what is this doing?
+})
+
+app.get('/weather', function (req, res) {
+	res.render('index')
+	// res.send("Hello this is working :)")
 });
+
 
 app.listen(3000, function () {
   console.log('Server is up nd running on localhost:3000!');
